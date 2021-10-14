40c8bfea297b73b1bbc9e34ac08cabe1f36e9d89
Hoàng Minh Huệ <httt2hauik15@gmail.com>
2021-10-14 20:51:14 +0700

    BTVN B1

---
+++ buoi1/HCN.java
@@ -0,0 +1,12 @@
+
+public class HCN{
+    public static void main(String[] args){
+        int m=10,n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
