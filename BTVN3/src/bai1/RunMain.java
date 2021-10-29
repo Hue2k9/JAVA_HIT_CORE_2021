75dcb9511b55fbee5869b9f1ba25f79c83bbdf27
Hoàng Minh Huệ <httt2hauik15@gmail.com>
2021-10-29 15:10:33 +0700

    BTVN 3

---
+++ BTVN3/src/bai1/RunMain.java
@@ -0,0 +1,7 @@
+package bai1;
+public class RunMain {
+    public static void main(String[] args){
+       Person person=new Person("Hoang Minh Hue",19,"nu","Chinese movies");
+       System.out.print(person.toString());
+    }
+}
